876feb40153e97f6f8fc0364b74d268c24ab2065
Yauheni Yarotski <yauheni_yarotski@me.com>
2019-05-18 13:18:48 +0300

    Initial Commit

---
+++ JKH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JKH
+//
+//  Created by Yauheni Yarotski on 5/18/19.
+//  Copyright © 2019 Yauheni Yarotski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
